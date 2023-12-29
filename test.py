a04294c919679c405fd65d7ae4b84545768bd8db
i-am-smf <fahadguy8@gmail.com>
2023-12-29 23:26:12 +0530

    'backup'

--- test.py
+++ test.py
@@ -4,4 +4,6 @@ import os
 os.system("git add .")
 os.system("git commit -m 'backup'")
 
+# git.Commit("https://github.com/i-am-smf/ticket-backup.git",message="backup")
+
 git.Git('i-am-smf/').push(f"https://github.com/i-am-smf/ticket-backup.git")
